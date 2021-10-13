4dc77162f761758c8a2a49d69e05487fb4a6eaea
Стефан Степанов <a1806430@yandex.ru>
2021-10-13 10:56:51 +0400

    первое задание готово

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 
-print('hello')
+import os
+import zipfile
+
+os.mkdir("unpacked")
+directory_to_extract_to = os.path.abspath("unpacked")
+arch_file = "D:\\archive.zip"
+zip_archive = zipfile.ZipFile(arch_file)
+zip_archive.extractall(directory_to_extract_to)
+zip_archive.close()
