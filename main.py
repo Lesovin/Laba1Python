d70d538aaff6394b4a096cfcb938a82c58ea34e8
Стефан Степанов <a1806430@yandex.ru>
2021-10-13 11:11:29 +0400

    настроил файл .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 import os
 import zipfile
 
-os.mkdir("unpacked")
+#os.mkdir("unpacked")
 directory_to_extract_to = os.path.abspath("unpacked")
 arch_file = "D:\\archive.zip"
 zip_archive = zipfile.ZipFile(arch_file)
 zip_archive.extractall(directory_to_extract_to)
 zip_archive.close()
+path=os.path.abspath(".idea")
+print(path)
