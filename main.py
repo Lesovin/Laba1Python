aa078029450b4abef3aa78296c61c7bc0e144f82
Стефан Степанов <a1806430@yandex.ru>
2021-10-13 13:46:18 +0400

    2.1 готово

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
 import os
 import zipfile
+import hashlib
 
-#os.mkdir("unpacked")
+# os.mkdir("unpacked")
 directory_to_extract_to = os.path.abspath("unpacked")
 arch_file = "D:\\archive.zip"
 zip_archive = zipfile.ZipFile(arch_file)
 zip_archive.extractall(directory_to_extract_to)
 zip_archive.close()
-path=os.path.abspath(".idea")
-print(path)
+txt_files=[]
+for root, dirs, files in os.walk(directory_to_extract_to):
+    for file in files:
+        if file.endswith(".txt"):
+            txt_files.append(os.path.join(root, file))
+print(txt_files)
+
+
